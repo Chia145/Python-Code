8700e41142f93811c0733d7fee6c070d6d529327
coderanishka <62501236+coderanishka@users.noreply.github.com>
2021-02-25 21:13:37 +0530

    Update bellCurve(ProC108).py

--- Project 108/bellCurve(ProC108).py
+++ Project 108/bellCurve(ProC108).py
@@ -3,6 +3,6 @@ import pandas as pd
 import csv
 
 df = pd.read_csv("data(C108).csv")
-fig = ff.create_distplot([df["Avg Rating"].tolist()], ["Avg Rating"])
+fig = ff.create_distplot([df["Avg Rating"].tolist()], ["Average Rating"])
 fig.show()
 
