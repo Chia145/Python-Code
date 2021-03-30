63b0f04f8e666dd81248586b7c26674d3da9d2c7
coderanishka <62501236+coderanishka@users.noreply.github.com>
2021-03-30 18:08:54 +0530

    Update samplingDistribution.py

--- Project 111/samplingDistribution.py
+++ Project 111/samplingDistribution.py
@@ -3,6 +3,7 @@ import statistics
 import random
 import pandas as pd
 import csv
+import plotly.graph_objects as go
 
 df = pd.read_csv("medium_data.csv")
 data = df["reading_time"].tolist()
@@ -21,26 +22,31 @@ def RandomSetOfMeans(counter):
     sampleMean = statistics.mean(dataset)
     return sampleMean
 
-def setup():
-    meanList = []
-    for i in range(0,100):
-        setOfMeans = RandomSetOfMeans(30)
-        meanList.append(setOfMeans)
+
+meanList = []
+for i in range(0,100):
+    setOfMeans = RandomSetOfMeans(30)
+    meanList.append(setOfMeans)
 
 
 #calculating the Standard Deviations 1,2,3.
-sd1Start = sampleMean - sd
-sd1End = sampleMean + sd
+sd1Start = RandomSetOfMeans(30) - sd
+sd1End = RandomSetOfMeans(30) + sd
 
-sd2Start = sampleMean - (sd*2)
-sd2End = sampleMean + (sd*2)
+sd2Start = RandomSetOfMeans(30) - (sd*2)
+sd2End = RandomSetOfMeans(30) + (sd*2)
 
-sd3Start = sampleMean - (sd*3)
-sd3End = sampleMean + (sd*3)
+sd3Start = RandomSetOfMeans(30) - (sd*3)
+sd3End = RandomSetOfMeans(30) + (sd*3)
 
-fig = ff.create_distplot([mean_list], ["student marks"], show_hist=False)
-fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN"))
-fig.add_trace(go.Scatter(x=[sampleMean, sampleMean], y=[0, 0.17], mode="lines", name="MEAN"))
+sampleMean = RandomSetOfMeans(30)
+
+fig = ff.create_distplot([meanList], ["student marks"], show_hist=False)
+fig.add_trace(go.Scatter(x=[Cmean, Cmean], y=[0, 0.6], mode="lines", name="MEAN"))
+fig.add_trace(go.Scatter(x=[sampleMean, sampleMean], y=[0, 0.6], mode="lines", name="MEAN"))
 fig.add_trace(go.Scatter(x=[sd2Start, sd2End], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2"))
 fig.add_trace(go.Scatter(x=[sd3Start, sd3End], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 3"))
 fig.show()
+
+zscore = (Cmean - sampleMean)/sd
+print("the z score is ", zscore) 
