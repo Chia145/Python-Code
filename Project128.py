8c546048b1a44c8947669ff6c2e4f2ac93bace98
coderanishka <62501236+coderanishka@users.noreply.github.com>
2021-06-10 09:23:09 +0530

    Update Project128.py

--- Project128.py
+++ Project128.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
+import csv
 
 url = "https://en.wikipedia.org/wiki/List_of_brown_dwarfs"
 
@@ -12,13 +13,13 @@ starTable = soup.find_all('table')
 # print(len(starTable)) 
 
 tempList = []
-tableRows = starTable[2].find_all
+tableRows = starTable[2].find_all('tr')
 
 for r in tableRows:
     td = r.find_all('td')
     row = [i.text.rstrip() for i in td]
     tempList.append(row)
-print(tempList)
+# print(tempList)
 
 Star_Names = []
 Distance = []
@@ -32,5 +33,9 @@ for i in range(1, len(tempList)):
     Radius.append(tempList[i][8])
 
 header = ['Star_name','Distance','Mass','Radius']
-df2 = pd.DataFrame(list(Star_Names, Distance, Mass, Radius),columns = header)
-df2.to_csv('DwarfStars.csv')
+
+with open('Dwarf_Stars.csv', 'w') as f:
+    cw = csv.writer(f)
+    cw.writerow(header)
+    cw.writerows(tempList)
+
